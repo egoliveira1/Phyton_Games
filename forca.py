88eeee722c79e29102cfda1992846cbd94077829
“egoliveira1” <eron.oliveira@gmail.com>
2022-11-06 18:23:23 +0000

    New logical, list letters.

--- forca.py
+++ forca.py
@@ -3,12 +3,22 @@ def play_forca():
     print('Welcome to the Hanged game! ****')
     print('********************************')
 
-    secret_word = 'banana'
-    result = ['_', '_', '_', '_', '_', '_']
+    file = open('words.txt', 'r')
+    words = []
+
+    for line in words:
+        line = line.strip()
+        words.append(line)
+
+    file.close()
+
+    secret_word = 'banana'.upper()
+    result = ['_' for letra in secret_word]
     number_of_letters = len(secret_word)
 
     hanged = False
     hits = False
+    errors = 0
 
     print(result)
     print('The word has {} letters.'.format(number_of_letters))
@@ -17,15 +27,26 @@ def play_forca():
         print('Playing')
 
         trying = input('What is the letter? ')
-        trying = trying.strip()
+        trying = trying.strip().upper()
 
-        index = 0
+        if(trying in secret_word):
+            index = 0
+            for letter in secret_word:
+                if(trying == letter):
+                    result[index] = trying
+                index += 1
+        else:
+            errors += 1
+            print('You made a mistake! You have {} more tries left.'.format(6-errors))
 
-        for letter in secret_word:
-            if(trying.upper() == letter.upper()):
-                result[index] = trying
-            index = index + 1
+        hanged = errors == 6
+        hits = "_" not in result
         print(result)
+
+    if(hits):
+        print('You win!')
+    else:
+        print('You lose!')
 
     print('Game over!')
 
